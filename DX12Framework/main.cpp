1f3bb2ef6d271795d61bbecb375ea70b9f2c6c27
GB_Game_Builder <garbagecanpoi@gmail.com>
2021-03-26 09:31:38 +0900

    【chapter 8】白テクスチャ作成関数の追加

--- DX12Framework/main.cpp
+++ DX12Framework/main.cpp
@@ -170,7 +170,58 @@ ID3D12Resource* LoadTextureFromFile(std::string& texPath) {
 	return texbuff;
 }
 
+// 白テクスチャの作成
+ID3D12Resource* CreateWhiteTexture() {
 
+	D3D12_HEAP_PROPERTIES texHeapProp = {};
+	texHeapProp.Type = D3D12_HEAP_TYPE_CUSTOM;
+	texHeapProp.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_WRITE_BACK;
+	texHeapProp.MemoryPoolPreference = D3D12_MEMORY_POOL_L0;
+	// texHeapProp.CreationNodeMask = 0;
+	texHeapProp.VisibleNodeMask = 0;
+
+	D3D12_RESOURCE_DESC resDesc = {};
+	resDesc.Format				= DXGI_FORMAT_R8G8B8A8_UNORM;
+	resDesc.Width				= 4;	// 幅
+	resDesc.Height				= 4;	// 高さ
+	resDesc.DepthOrArraySize	= 1;
+	resDesc.SampleDesc.Count	= 1;
+	resDesc.SampleDesc.Quality	= 0;
+	resDesc.MipLevels			= 1;
+	resDesc.Dimension			= D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+	resDesc.Layout				= D3D12_TEXTURE_LAYOUT_UNKNOWN;
+	resDesc.Flags				= D3D12_RESOURCE_FLAG_NONE;
+
+	ID3D12Resource* whiteBuff = nullptr;
+
+	auto result = _dev->CreateCommittedResource(
+		&texHeapProp,
+		D3D12_HEAP_FLAG_NONE,	// 特に指定なし
+		&resDesc,
+		D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE,
+		nullptr,
+		IID_PPV_ARGS(&whiteBuff)
+	);
+
+	if (FAILED(result))
+	{
+		return nullptr;
+	}
+
+	std::vector<unsigned char> data(4 * 4 * 4);
+	std::fill(data.begin(), data.end(), 0xff);	// 全部255で埋める
+
+	// データ転送
+	result = whiteBuff->WriteToSubresource(
+		0,
+		nullptr,
+		data.data(),
+		4 * 4,
+		data.size()
+	);
+
+	return whiteBuff;
+}
 
 void EnableDebugLayer() {
 	ID3D12Debug* debugLayer = nullptr;
