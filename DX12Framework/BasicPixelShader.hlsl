558df4c89085caa3b2444ed49ff11f872ea522a7
GB_Game_Builder <garbagecanpoi@gmail.com>
2021-03-26 09:17:18 +0900

    【chapter 8】ピクセルシェーダーの変更（テクスチャ以外真っ黒）

--- DX12Framework/BasicPixelShader.hlsl
+++ DX12Framework/BasicPixelShader.hlsl
@@ -18,7 +18,10 @@ float4 BasicPS(BasicType input) : SV_TARGET{
 	float3 light = normalize(float3(1,-1,1));
 	float brightness = dot(-light, input.normal);
 
-	return float4(brightness, brightness, brightness, 1) * diffuse;
+	return 
+		float4(brightness, brightness, brightness, 1) 
+		* diffuse
+		* tex.Sample(smp,input.uv);
 	//return float4(input.normal.xyz,1);
 	//return float4(tex.Sample(smp,input.uv));
 }
