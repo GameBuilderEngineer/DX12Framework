e11b686f7efebf76fe9276dded6ee4ed4bde109b
GB_Game_Builder <garbagecanpoi@gmail.com>
2021-03-27 18:28:16 +0900

    【chapter 8】ピクセルシェーダーの演算結果に加算スフィアマップを追加

--- DX12Framework/BasicPixelShader.hlsl
+++ DX12Framework/BasicPixelShader.hlsl
@@ -1,6 +1,7 @@
 #include "BasicType.hlsli"
 Texture2D<float4> tex:register(t0);		// 0番スロットに設定されたテクスチャ(ベース)
 Texture2D<float4> sph:register(t1);		// 1番スロットに設定されたテクスチャ(乗算)
+Texture2D<float4> spa:register(t2);		// 1番スロットに設定されたテクスチャ(加算)
 
 SamplerState smp:register(s0);			// 0番スロットに設定されたサンプラ
 
@@ -24,9 +25,10 @@ float4 BasicPS(BasicType input) : SV_TARGET{
 	float2 sphereMapUV = input.normal.xy;
 	sphereMapUV = (sphereMapUV + float2(1, -1)) * float2(0.5, -0.5);
 
-	return 
+	return
 		float4(brightness, brightness, brightness, 1)	// 輝度
 		* diffuse										// ディフューズ色
-		* tex.Sample(smp,input.uv)						// テクスチャカラー
-		* sph.Sample(smp, sphereMapUV);					// スフィアマップ(乗算)
+		* tex.Sample(smp, input.uv)						// テクスチャカラー
+		* sph.Sample(smp, sphereMapUV)					// スフィアマップ(乗算)
+		+ spa.Sample(smp, sphereMapUV);					// スフィアマップ(乗算)
 }
