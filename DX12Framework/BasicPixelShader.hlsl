18fcb209eaf4163cb252c8fe273d2c04b4ecaa77
GB_Game_Builder <garbagecanpoi@gmail.com>
2021-03-27 17:31:38 +0900

    【chapter 8】ピクセルシェーダー演算結果にスフィアマップを追加

--- DX12Framework/BasicPixelShader.hlsl
+++ DX12Framework/BasicPixelShader.hlsl
@@ -1,6 +1,8 @@
 #include "BasicType.hlsli"
-Texture2D<float4> tex:register(t0);		//0番スロットに設定されたテクスチャ
-SamplerState smp:register(s0);			//0番スロットに設定されたサンプラ
+Texture2D<float4> tex:register(t0);		// 0番スロットに設定されたテクスチャ(ベース)
+Texture2D<float4> sph:register(t1);		// 1番スロットに設定されたテクスチャ(乗算)
+
+SamplerState smp:register(s0);			// 0番スロットに設定されたサンプラ
 
 // 定数バッファ0
 cbuffer cbuff0 : register(b0) {
@@ -18,10 +20,13 @@ float4 BasicPS(BasicType input) : SV_TARGET{
 	float3 light = normalize(float3(1,-1,1));
 	float brightness = dot(-light, input.normal);
 
+	// スフィアマップ用UV
+	float2 sphereMapUV = input.normal.xy;
+	sphereMapUV = (sphereMapUV + float2(1, -1)) * float2(0.5, -0.5);
+
 	return 
-		float4(brightness, brightness, brightness, 1) 
-		* diffuse
-		* tex.Sample(smp,input.uv);
-	//return float4(input.normal.xyz,1);
-	//return float4(tex.Sample(smp,input.uv));
+		float4(brightness, brightness, brightness, 1)	// 輝度
+		* diffuse										// ディフューズ色
+		* tex.Sample(smp,input.uv)						// テクスチャカラー
+		* sph.Sample(smp, sphereMapUV);					// スフィアマップ(乗算)
 }
