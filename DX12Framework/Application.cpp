5084328c7c9eab644a26a3b6304a2b8346f399a2
GB_Game_Builder <garbagecanpoi@gmail.com>
2021-04-12 02:21:26 +0900

    【chapter 9】void DebugOutputFormatStringの移動

--- DX12Framework/Application.cpp
+++ DX12Framework/Application.cpp
@@ -15,6 +15,17 @@ using namespace DirectX;
 using namespace std;
 using namespace Microsoft::WRL;
 
+///@brief コンソール画面にフォーマット付き文字列を表示
+///@param format フォーマット
+///@param 可変長引数
+void DebugOutputFormatString(const char* format, ...) {
+#ifdef _DEBUG
+	va_list valist;
+	va_start(valist, format);
+	vprintf(format, valist);
+	va_end(valist);
+#endif
+}
 
 void Application::Run()
 {
--- DX12Framework/main.cpp
+++ DX12Framework/main.cpp
@@ -27,17 +27,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 //	p = nullptr;
 //}
 //
-/////@brief コンソール画面にフォーマット付き文字列を表示
-/////@param format フォーマット
-/////@param 可変長引数
-//void DebugOutputFormatString(const char* format, ...) {
-//#ifdef _DEBUG
-//	va_list valist;
-//	va_start(valist, format);
-//	vprintf(format, valist);
-//	va_end(valist);
-//#endif
-//}
 //
 //LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
 //	if (msg == WM_DESTROY) {
