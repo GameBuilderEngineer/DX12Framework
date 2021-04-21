01cf2b8c5ddb2139745980f6f7812ecca8ebcea9
GB_Game_Builder <garbagecanpoi@gmail.com>
2021-04-22 02:38:11 +0900

    【chapter 9】PMDActorにTransformを追加

--- DX12Framework/PMDActor.cpp
+++ DX12Framework/PMDActor.cpp
@@ -1,1 +1,6 @@
 ﻿#include "PMDActor.h"
+
+void* PMDActor::Transform::operator new(size_t size)
+{
+	return _aligned_malloc(size, 16);
+}
--- DX12Framework/PMDActor.h
+++ DX12Framework/PMDActor.h
@@ -1,5 +1,20 @@
 ﻿#pragma once
+
+#include<d3d12.h>
+#include<DirectXMath.h>
+#include<vector>
+#include<wrl.h>
+
 class PMDActor
 {
+private:
+	struct Transform
+	{
+		// 内部に持っているXMMATRIXメンバが16バイトアライメントであるため
+		// Transformをnewする際には16バイト境界に確保する
+		void* operator new(size_t size);
+		DirectX::XMMATRIX world;
+	};
+
 };
 
